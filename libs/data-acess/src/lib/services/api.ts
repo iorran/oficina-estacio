28a1fb6b1fe24bcf14da4e6863378febfca60951
Iorran <iorran.castro@vakt.io>
2020-10-24 22:21:48 +0100

    (feat) giving credentials

--- libs/data-acess/src/lib/services/api.ts
+++ libs/data-acess/src/lib/services/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://my-json-server.typicode.com/iorran/db-palestra-estacio/"
+  baseURL: "https://my-json-server.typicode.com/iorran/db-palestra-estacio",
+  headers:  {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  withCredentials: true
 });
